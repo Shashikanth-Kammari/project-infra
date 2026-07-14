27f1bf3782a3e16d8d4dbfa99f05e58c1e67b2d4
Shashi <shashikanth0312@gmail.com>
2026-07-15 00:00:41 +0530

    adding

---
+++ ec2/ec2.tf
@@ -0,0 +1,26 @@
+# resource <resource type> and <resource name>
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh"
+    description =  "allowing SSH access"
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port        = 0  # from 0 to 0 means, pening all protocols 
+        to_port          = 0
+        protocol         = "-1"  # -1 all protocols
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+
+    tags = {
+        Name = "allow_ssh"
+        createdby = "shashikanth"
+    }
+
+     
+}
