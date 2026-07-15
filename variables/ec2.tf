bb2564407c5ab385123c28cd0b46f794af08a801
Shashi <shashikanth0312@gmail.com>
2026-07-15 17:50:01 +0530

    adding

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -15,17 +15,17 @@ resource "aws_security_group" "allow_ssh" {
     name = var.sg_name
     description =  var.sg_description
     ingress {
-        from_port        = 22
-        to_port          = 22
-        protocol         = "tcp"
-        cidr_blocks      = ["0.0.0.0/0"]
+        from_port        = var.ssh_port
+        to_port          = var.ssh_port
+        protocol         = var.protocol
+        cidr_blocks      = var.cidr_block
     }
 
     egress {
         from_port        = 0  # from 0 to 0 means, pening all protocols 
         to_port          = 0
         protocol         = "-1"  # -1 all protocols
-        cidr_blocks      = ["0.0.0.0/0"]
+        cidr_blocks      = var.cidr_block
     }
 
     tags = {
