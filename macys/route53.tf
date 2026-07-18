55fb951f97aa190ba9d411a2a52ecf499173fc83
Shashi <shashikanth0312@gmail.com>
2026-07-18 15:50:39 +0530

    adding

--- macys/route53.tf
+++ macys/route53.tf
@@ -1,8 +1,8 @@
-resource "aws_route53_record" "expence" {
+resource "aws_route53_record" "macys" {
   count = length(var.instance_name)
   zone_id = var.zone_id
   name    = var.instance_name[count.index] == "frontend" ? var.domain_name : "${var.instance_name[count.index]}.${var.domain_name}"
   type    = "A"
   ttl     = 1
-  records = var.instance_name[count.index] == "frontend" ? [aws_instance.expence[count.index].public_ip]: [aws_instance.expence[count.index].private_ip]
+  records = var.instance_name[count.index] == "frontend" ? [aws_instance.macys[count.index].public_ip]: [aws_instance.macys[count.index].private_ip]
 }
