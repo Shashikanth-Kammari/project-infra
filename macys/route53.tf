832ed6931ae8a8118da4ea5644b1ebe5f0b05586
Shashi <shashikanth0312@gmail.com>
2026-07-18 15:41:55 +0530

    adding

--- macys/route53.tf
+++ macys/route53.tf
@@ -1,7 +1,8 @@
-resource "aws_route53_record" "dev-ns" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "dev.example.com"
+resource "aws_route53_record" "expence" {
+  count = length(var.instance_name)
+  zone_id = var.zone_id
+  name    = var.instance_name[count.index] == "frontend" ? var.domain_name : "${var.instance_name[count.index]}.${var.domain_name}"
   type    = "NS"
-  ttl     = "30"
-  records = aws_route53_zone.dev.name_servers
+  ttl     = 1
+  records = var.instance_name[count.index] == "frontend" ? [aws_instance.expence[count.index].public_ip]: [aws_instance.expence[count.index].private_ip]
 }
