79564afb903b85d5460cc803ad78be0ce9ecb0d4
Shashi <shashikanth0312@gmail.com>
2026-07-18 17:28:20 +0530

    adding

---
+++ data_sources/data.tf
@@ -0,0 +1,16 @@
+data "aws_ami" "ami_id" {
+  most_recent = true
+  owners = ["620549678005"]
+  filter {
+    name = "name"
+    values = ["Devops"]
+  }
+  filter {
+    name = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
