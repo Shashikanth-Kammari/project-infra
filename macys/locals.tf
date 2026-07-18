6b5cdab9b0f499a93f2385ba607178475cb9a7b2
Shashi <shashikanth0312@gmail.com>
2026-07-19 01:55:17 +0530

    adding

--- macys/locals.tf
+++ macys/locals.tf
@@ -1,5 +1,5 @@
-locals {
-  instance_name = var.instance_names[count.index] == "db" ? "t3.small" : "t3.micro"
-  record_name = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}"
-  records_value = var.instance_names[count.index] == "frontend" ? [aws_instance.expense[count.index].public_ip] : [aws_instance.expense[count.index].private_ip]
-}
+
+ #count and count.index will not work in locals
+# locals {
+#   #instance_name = var.instance_names[count.index] == "db" ? "t3.small" : "t3.micro"  
+# }
