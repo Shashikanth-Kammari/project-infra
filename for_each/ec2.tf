b18a1e5b3bd73b56afb8791a373086e604160496
Shashi <shashikanth0312@gmail.com>
2026-07-19 11:01:31 +0530

    adding

--- for_each/ec2.tf
+++ for_each/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "expense" {
     for_each = var.instance_names
     ami = var.ami-id
     vpc_security_group_ids = var.vpc_security_group_id
-    instance_type = each.value
+    instance_type = each.value == "db" ? "t3.small" : "t3.micro"
     tags = merge(
         var.common_tags,
         {
