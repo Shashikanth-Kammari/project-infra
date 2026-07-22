dbc9243490253334a83d39d6d6ec4e22c2c66e02
Shashi <shashikanth0312@gmail.com>
2026-07-22 23:32:53 +0530

    adding

--- terraform-aws-vpc/variables.tf
+++ terraform-aws-vpc/variables.tf
@@ -60,7 +60,7 @@ variable "private_subnet_cidrs" {
   type = list
   validation {
     condition = length(var.private_subnet_cidrs) == 2
-    error_message = "please provide 2 valid public subnet CIDR"
+    error_message = "please provide 2 valid private subnet CIDR"
   }
 }
 
@@ -74,7 +74,7 @@ variable "database_subnet_cidrs" {
   type = list
   validation {
     condition = length(var.database_subnet_cidrs) == 2
-    error_message = "please provide 2 valid public subnet CIDR"
+    error_message = "please provide 2 valid databse subnet CIDR"
   }
 }
 
