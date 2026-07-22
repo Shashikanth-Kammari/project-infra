2d18dd60d7b808222f962065ead110eee4ec37e1
Shashi <shashikanth0312@gmail.com>
2026-07-22 20:59:42 +0530

    adding

--- terraform-aws-vpc/vpc.tf
+++ terraform-aws-vpc/vpc.tf
@@ -27,6 +27,8 @@ resource "aws_internet_gateway" "gw" {
 ### public subnet 
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
 
