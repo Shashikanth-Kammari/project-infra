4468766922cd943b6c7f35ea6ffbf5dd17db24cb
Shashi <shashikanth0312@gmail.com>
2026-07-24 00:07:41 +0530

    adding

--- terraform-aws-vpc/peering.tf
+++ terraform-aws-vpc/peering.tf
@@ -35,3 +35,11 @@ resource "aws_route" "database_peering" {
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
 }
+
+
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required && var.accepter_vpc_id == "" ? 1 : 0
+  route_table_id            = data.aws_route_table.main.id     #default vpc route table
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
+}
