34cf068fdeded9fee685b5b398465c45fb8a1761
Shashi <shashikanth0312@gmail.com>
2026-07-23 22:54:37 +0530

    adding

--- terraform-aws-vpc/peering.tf
+++ terraform-aws-vpc/peering.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc_peering_connection" "peering" {
+  count = var.is_peering_required ? 1 : 0
   peer_owner_id = var.peer_owner_id
   peer_vpc_id   = aws_vpc.bar.id
   vpc_id        = aws_vpc.foo.id
