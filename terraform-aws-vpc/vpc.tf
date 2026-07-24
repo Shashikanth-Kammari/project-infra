de5833cf11b988020157d23343f1c1ca0e7160c0
Shashi <shashikanth0312@gmail.com>
2026-07-24 22:36:27 +0530

    adding

--- terraform-aws-vpc/vpc.tf
+++ terraform-aws-vpc/vpc.tf
@@ -177,7 +177,11 @@ resource "aws_route" "database_route_nat" {
 
 #### route table and subnet associations ####
 
-
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public.id
+}
 
 resource "aws_route_table_association" "private" {
   count = length(var.private_subnet_cidrs)
